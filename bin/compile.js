86a6fc69bb068a4ebcc23c294a8bc791d37521c7
Thomas Shaw <printer.83mph@gmail.com>
2019-10-09 17:15:39 -0400

    Test

--- bin/compile.js
+++ bin/compile.js
@@ -3,9 +3,7 @@ const fs = require('fs');
 
 var result = sass.renderSync({
     file: "src/scss/main.scss",
-    outputStyle: "compressed",
-    outFile: "styles.css"
+    outputStyle: "compressed"
 });
 
 fs.writeFileSync("src/css/styles.css", result.css);
-fs.writeFileSync("src/css/styles.css.map", result.map);
