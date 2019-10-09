8ed3f6e65acd4150e765cd636e55e711c9f7e661
Thomas Shaw <printer.83mph@gmail.com>
2019-10-09 17:40:11 -0400

    Huh

--- bin/compileDev.js
+++ bin/compileDev.js
@@ -1,11 +1,14 @@
 const sass = require('sass');
 const fs = require('fs');
 
+// TODO: fix this
+
 var result = sass.renderSync({
     file: "scss/main.scss",
-    sourceMap: true,
+    outFile: "css/style.css",
+    sourceMap: "css/style.css.map",
     outputStyle: "expanded"
 });
 
 fs.writeFileSync("public/css/styles.css", result.css);
-fs.writeFileSync("public/css/main.scss.map", result.map);
+fs.writeFileSync("public/css/style.scss.map", result.map);
